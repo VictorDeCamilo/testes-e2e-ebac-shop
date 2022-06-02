5f005be884d25e96c5c78be1691f1702c4869746
Victor De Camilo <victor.camilo@finnet.com.br>
2022-06-02 13:02:10 -0300

    Exercicio e2e

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+const faker = require ('faker')
+
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     /*  Como cliente 
         Quero acessar a Loja EBAC 
@@ -9,12 +11,54 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         Preenchendo todas opções no checkout
         E validando minha compra ao final */
 
+    const nome = faker.name.firstName()
+    const sobrenome = faker.name.lastName()
+    const email = faker.internet.email(nome)
+
     beforeEach(() => {
-        cy.visit('/')
+        cy.visit('minha-conta')
+        cy.preCadastro(email, nome, sobrenome)
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
+        const meioDePagamento = 'Transferência bancária'
+        
+        cy.visit('produtos')
+
+        cy.addProdutos('Abominable Hoodie', 'XS', 'Red', 3)
+        cy.validaAddProduto(3, 'Abominable Hoodie')
+        cy.get('#primary-menu > .menu-item-629 > a').click()
+
+        cy.addProdutos('Aero Daily Fitness Tee', 'S', 'Brown', 1)
+        cy.validaAddProduto(4, 'Aero Daily Fitness Tee')
+        cy.get('#primary-menu > .menu-item-629 > a').click()
+
+        cy.addProdutos('Aether Gym Pant', '33', 'Green', 4)
+        cy.validaAddProduto(8, 'Aether Gym Pant')
+        cy.get('#primary-menu > .menu-item-629 > a').click()
+
+        cy.get('[class="product-block grid"]')
+            .eq(3)
+            .click()
+        cy.get('.single_add_to_cart_button').click()
+        cy.validaAddProduto(9, 'Agasalho jhony quest')
+
+        cy.get('.woocommerce-message > .button').click()
+
+        cy.get('strong > .woocommerce-Price-amount > bdi').then(valorTotal => {
+            const valor = valorTotal.text()
+            expect(valor).to.eq('R$856,00')
+        })
+
+        cy.get('.checkout-button').click()
+        cy.editarEndFaturamento('Brasil', 'Rua Itambé', '123', 'São Paulo', 'São Paulo', '01222111', '11988888983')
+        cy.contains(meioDePagamento).click()
+        cy.get('#terms').click()
+        cy.get('#place_order').click()
+
+        cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
+        cy.get('tfoot > :nth-child(3) > td > .woocommerce-Price-amount').should('have.text', 'R$856,00')
+        cy.get('tfoot > :nth-child(2) > td').should('have.text', meioDePagamento)    
     });
 
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,29 +1,4 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
+/// <reference types = "cypress" />
 
 Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('#username').type(usuario)
@@ -31,3 +6,42 @@ Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('.woocommerce-form > .button').click()
 });
 
+Cypress.Commands.add('preCadastro', (email, nome, sobrenome) =>{
+    cy.get('#reg_email').clear().type(email)
+    cy.get('#reg_password').clear().type('senha222senha')
+    cy.get('.button[name="register"]').should('be.visible').click()
+
+    cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a')
+        .click()
+
+    cy.get('#account_first_name').clear().type(nome)
+    cy.get('#account_last_name').clear().type(sobrenome)
+    cy.get('.woocommerce-Button').should('be.visible').click()
+});
+
+Cypress.Commands.add('addProdutos', (nomeProduto, tamanho, cor, quantidade) => {
+    cy.get('[class="product-block grid"]')
+        .contains(nomeProduto)
+        .click()
+    
+    cy.get('.button-variable-item-' + tamanho).click()
+    cy.get('.button-variable-item-' + cor).click()
+    cy.get('.input-text').clear().type(quantidade)
+    cy.get('.single_add_to_cart_button').click()
+});
+
+Cypress.Commands.add('validaAddProduto', (quantCarrinho, nomeProd) => {
+    cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantCarrinho)
+    cy.get('.woocommerce-message').should('contain', nomeProd)
+});
+
+Cypress.Commands.add('editarEndFaturamento', (pais, endereco, numeroEnd, cidade, estado, cep, telefone) => {
+    cy.get('#select2-billing_country-container').click()
+    .type(pais).get('[aria-selected="true"]').click()
+    cy.get('#billing_address_1').clear().type(endereco)
+    cy.get('#billing_address_2').clear().type(numeroEnd)
+    cy.get('#billing_city').clear().type(cidade)
+    cy.get('#select2-billing_state-container').click().type(`${estado}{enter}`)
+    cy.get('#billing_postcode').clear().type(cep)
+    cy.get('#billing_phone').clear().type(telefone)
+});
